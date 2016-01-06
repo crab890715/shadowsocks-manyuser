7af1154bc04a4c35d4ce0f8b4af3102cf0b2b7fe
liuwanjie <530680853@qq.com>
2016-01-06 22:28:56 +0800

    添加vip限制

--- shadowsocks/db_transfer.py
+++ shadowsocks/db_transfer.py
@@ -111,6 +111,9 @@ class DbTransfer(object):
                     #password changed
                     logging.info('db stop server at port [%s] reason: password changed' % (row[0]))
                     ServerPool.get_instance().del_server(row[0]) 
+                if Config.SERVER_TYPE=='VIP' and row[7]==0:
+                    ServerPool.get_instance().del_server(row[0])
+                    
             else:
                 if row[5] == 1 and row[6] == 1 :
                     
