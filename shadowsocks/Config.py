64315b012edd18fa0508631b5a303ad5b19cb1d6
liuwanjie <530680853@qq.com>
2016-01-06 20:50:23 +0800

    添加vip限制

--- shadowsocks/Config.py
+++ shadowsocks/Config.py
@@ -8,7 +8,7 @@ MYSQL_PASS = 'password'
 MYSQL_DB = 'ss'
 #对应ss_node表的id
 SERVER_ID = 1
-#节点类型：FREE为免费，VIP为付费节点
+#节点类型：FREE为免费，VIP为付费节点,必须大写
 SERVER_TYPE= 'FREE'
 MANAGE_PASS = 'ss233333333'
 #if you want manage in other server you should set this value to global ip
