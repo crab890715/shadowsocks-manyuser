f8eadafa2dccff47ce4594f7a87319879538ed91
liuwanjie <530680853@qq.com>
2016-01-20 20:52:33 +0800

    修改流量监控逻辑

--- shadowsocks/day_task.py
+++ shadowsocks/day_task.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 from db import MySQL
-import cymysql
-def service(uid,type):
+import cymysql,time,datetime,calendar
+def get_service(uid,types):
     conn = MySQL.db()
+    today = time.strftime("%Y-%m-%d")
     cur = conn.cursor(cursor=cymysql.cursors.DictCursor)
-    cur.execute("SELECT * FROM t_user_service  where state=1 and uid=%s and service_type=%s",[uid,type])
+    cur.execute("SELECT * FROM t_user_service  where state=1 and uid=%s and service_type in %s and end_date>=%s order by end_date ASC limit 1",[uid,types,today])
     data = cur.fetchone()
     cur.close()
     conn.close()
-    pass
+    return data
 def run():
     conn = MySQL.db()
     cur = conn.cursor(cursor=cymysql.cursors.DictCursor)
     cur.execute("SELECT * FROM user where switch=1 and enable=1")
+    today = datetime.datetime.now()
+    days = calendar.monthrange(today.year, today.month)[1]
+    print days
     for user in cur.fetchall():
-        service(user['uid'],user['service_type'])
-        pass
+        service = get_service(user['uid'],[1,2])
+        if user['service_type'] in [1,2] :
+            if service :
+                start_day = service['start_date'].day
+                end_day = service['end_date'].day
+                print end_day
+        if user['service_type'] in [0,3] :
+            print service and service['end_date'].day
     cur.close()
     conn.close()
 if __name__ == '__main__':
